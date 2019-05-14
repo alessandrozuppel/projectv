e30c752a714d26bb25846c16aeceb188b1eaa58e
nicologarolla <47629675+nicologarolla@users.noreply.github.com>
2019-05-14 15:30:10 +0200

    Update main.js
    Aggiornato con aggiunta del collegamento ad InfluxDb

--- API/main.js
+++ API/main.js
@@ -6,43 +6,42 @@ const fastify = require('fastify')({
 fastify.register(require('fastify-cors'));
 
 const Influx = require('influx');
+
 const influx = new Influx.InfluxDB({
 host: 'localhost',
 database: 'busdati',
-port:8086,
-schema: [
- {
-   measurement: 'Posizione',
-   fields: {
-     latitudine: Influx.FieldType.STRING,
-     longitudine: Influx.FieldType.STRING
-   },
-   tags: [
-     'idBus'
-   ]
- }
-]
+port:8086
 })
-influx.writePoints([
-{
-  measurement: 'Posizione',
-  tags: { idBus: 2222 },
-  fields: { latitudine:22.2222, longitudine:22.2222 },
-}
-]).then(() => {
-return influx.query(`
-  select * from Posizione
-`)
-})/*.then(rows => {
-rows.forEach(row => console.log(`A request to ${row.path} took ${row.duration}ms`))
-})
-*/
 fastify.post('/api/busdati/', async (request, reply) => {
   var dati=request.body;
-
-
-
-  console.log("Dati: ",dati);
+  //console.log("Dati: ",dati);
+  if(dati.Apertura==''){
+    influx.writePoints([
+      {
+        measurement: 'Posizione',
+        tags: { idBus: dati.IdVeicolo },
+        fields: { latitudine:dati.Latitudine, longitudine:dati.Longitudine },
+        timestamp:dati.DataOraBus+"00",
+      }
+      ]);
+  }
+  else{
+    var porta;
+    if(dati.Apertura==1){
+       porta='Aperta';
+    }
+    else{
+      porta='Chiusa';
+    }
+    influx.writePoints([
+      {
+        measurement: 'Apertura',
+        tags: { idBus: dati.IdVeicolo },
+        fields: { Apertura:porta, latitudine:dati.Latitudine, longitudine:dati.Longitudine },
+        timestamp:dati.DataOraPorte+"00",
+      }
+      ]);
+  }
 
   reply.status(201).send(dati);
 
@@ -52,7 +51,7 @@ fastify.post('/api/busdati/', async (request, reply) => {
 
 const start = async () => {
   try {                                                       
-    await fastify.listen(3000,'192.168.101.81')                                                    //Creo web server e sto in ascolto sulla porta 3000
+    await fastify.listen(3000,'0.0.0.0')                                                    //Creo web server e sto in ascolto sulla porta 3000
     fastify.log.info(`server listening on ${fastify.server.address().port}`)      // Ascolto tutte richiest http
   } catch (err) {
     fastify.log.error(err)
