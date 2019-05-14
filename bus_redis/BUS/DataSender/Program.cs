52436391903cebb546dfde2f39e13cf695bdc3af
Alessandro Zuppel <47629733+alessandrozuppel@users.noreply.github.com>
2019-05-14 16:32:21 +0200

    Update Program.cs

--- bus_redis/BUS/DataSender/Program.cs
+++ bus_redis/BUS/DataSender/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 using System.IO;
 using CSRedis;
@@ -16,24 +16,56 @@ namespace DataSender
             {
                 // read from Redis queue
                 string json = redis.BLPop(30, "sensors_data");
-                Console.WriteLine(redis.BLPop(30, "sensors_data"));
+                //Console.WriteLine(redis.BLPop(30, "sensors_data"));
 
                 // send value to remote API
                 try
                 {
-                    var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://192.168.101.81:3000/api/busdati");
+                    var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://192.168.101.23:3000/api/busdati");
+                    //var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://127.0.0.2:5000/api/busdati/");
+                    httpWebRequest.ContentLength = json.Length;
                     httpWebRequest.ContentType = "application/json";
                     httpWebRequest.Method = "POST";
+                    httpWebRequest.Proxy = null;
                     using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                     {
-                        streamWriter.Write(json);
-                        streamWriter.Flush();
-                        streamWriter.Close();
+                        try
+                        {
+                            streamWriter.Write(json);
+                            streamWriter.Flush();
+                            streamWriter.Close();
+                        }
+                        catch(Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
                     }
+
+                    //Read Response
+                    //HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse();
+                    //WebHeaderCollection header = response.Headers;
+
+                    //using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
+                    //{
+                    //    string responseText = reader.ReadToEnd();
+                    //    Console.WriteLine(responseText);
+                    //}
+
+                    Console.Write("si ");
+                    for(int i=97; i<113; i++)
+                        {
+                            Console.Write(json[i]);
+                        }
+                    Console.WriteLine();
                 }
                 catch (Exception err)
                 {
-                    Console.WriteLine(err);
+                    Console.Write("no ");
+                    for(int i=97; i<113; i++)
+                        {
+                            Console.Write(json[i]);
+                        }
+                    Console.WriteLine();
                 }
             }
         }
