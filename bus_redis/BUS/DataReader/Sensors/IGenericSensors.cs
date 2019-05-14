43a99113f067df47b923d00e7674f803ab0dc6ed
Alessandro Zuppel <47629733+alessandrozuppel@users.noreply.github.com>
2019-05-14 16:46:49 +0200

    Update IGenericSensors.cs

--- bus_redis/BUS/DataReader/Sensors/IGenericSensors.cs
+++ bus_redis/BUS/DataReader/Sensors/IGenericSensors.cs
@@ -8,11 +8,11 @@ namespace DataReader.Sensors
 {
     interface IGenericSensors
     {
-        string Apertura_Chiusura(string Apertura);
-        string Conta_Persone(string CP);
-        string Posizione(string LAT, string LON);
-        string Id(string Id);
-        string oraapertura(string oraapertura);
-        string oraposizione(string oraposizione);
+        //string Apertura_Chiusura(string Apertura);
+        //string Conta_Persone(string CP);
+        //string Posizione(string POS);
+        //string Id(string Id);
+        //string oraapertura(string oraapertura);
+        //string oraposizione(string oraposizione);
     }
 }
