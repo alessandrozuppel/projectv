1fd91db3c144ec3421a59b0cd612e0d511a3b719
Alessandro Zuppel <47629733+alessandrozuppel@users.noreply.github.com>
2019-06-06 16:49:32 +0200

    Update main.js

--- API/main.js
+++ API/main.js
@@ -8,6 +8,11 @@ fastify.register(require('fastify-cors'));
 const Influx = require('influx');
 const fs=require('fs');
 
+fastify.register(require("fastify-jwt"), {
+  secret: 'supersecret'
+});
+const bcrypt = require("bcrypt");
+
 var obj= JSON.parse(fs.readFileSync('../config/configApi_Influxdb.json','utf8'));
 
 const influx = new Influx.InfluxDB({
@@ -15,6 +20,41 @@ host: obj.influx.host,
 database: obj.influx.database,
 port:obj.influx.port
 })
+
+fastify.post('/token', async(request,reply) => {
+  try{
+  let model= request.body;
+  if(model.password== "passwordsicura"){
+      var user= {
+          id: 10,
+          user: "pippo"
+      };
+      const token= fastify.jwt.sign({payload: user});
+      reply.send(token);
+      }
+  else{
+      reply.status(404).send({
+          "Error": "Username o Password sbagliati"
+      });
+      }
+
+  }
+  catch(err){
+      reply.send(err);
+  }
+  });
+
+
+fastify.register(async function(fastify, opts){
+  fastify.addHook('preHandler', async(request, reply) => {
+      try{
+          let model=request.body;
+          await request.jwtVerify(model.Token);
+      }
+      catch(err){
+          reply.send(err);
+      }
+  });
 
 // Lettura dati dal simulatore e salvataggio su influx DB
 fastify.post('/api/busdati/', async (request, reply) => {
@@ -51,6 +91,7 @@ fastify.post('/api/busdati/', async (request, reply) => {
   reply.status(201).send("201");
 
   
+  });
 });
 
 // visualizzazione dei dati 
