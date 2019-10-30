b0cdabdeba59e76fee3740ab4d0e55d4fee79880
nicologarolla <47629675+nicologarolla@users.noreply.github.com>
2019-10-30 09:11:44 +0100

    API definitive

--- API/main.js
+++ API/main.js
@@ -1,4 +1,4 @@
-const fastify = require('fastify')({ 
+const fastify = require('fastify')({
   logger: true,                           //quando arriva una richiesta http riesce a loggare qualsiasi richiesta, senza che ogni volta lo facciamo noi (il log)
   ignoreTrailingSlash: true               // Se =True --> tratta la richiesta con "/" finale o senza, allo stesso modo ("products/"" o "product")
 });
@@ -6,114 +6,151 @@ const fastify = require('fastify')({
 fastify.register(require('fastify-cors'));
 
 const Influx = require('influx');
-const fs=require('fs');
+const fs = require('fs');
 
 fastify.register(require("fastify-jwt"), {
   secret: 'supersecret'
 });
 const bcrypt = require("bcrypt");
 
-var obj= JSON.parse(fs.readFileSync('../config/configApi_Influxdb.json','utf8'));
+var obj = JSON.parse(fs.readFileSync('../config/configApi_Influxdb.json', 'utf8'));
 
 const influx = new Influx.InfluxDB({
-host: obj.influx.host,
-database: obj.influx.database,
-port:obj.influx.port
+  host: obj.influx.host,
+  database: obj.influx.database,
+  port: obj.influx.port
 })
 
-fastify.post('/token', async(request,reply) => {
-  try{
-  let model= request.body;
-  if(model.password== "passwordsicura"){
-      var user= {
-          id: 10,
-          user: "pippo"
+fastify.post('/token', async (request, reply) => {
+  try {
+    let model = request.body;
+    if (model.password == "passwordsicura") {
+      var user = {
+        id: 10,
+        user: "pippo"
       };
-      const token= fastify.jwt.sign({payload: user});
+      const token = fastify.jwt.sign({ payload: user });
       reply.send(token);
-      }
-  else{
+    }
+    else {
       reply.status(404).send({
-          "Error": "Username o Password sbagliati"
+        "Error": "Username o Password sbagliati"
       });
-      }
+    }
 
   }
-  catch(err){
+  catch (err) {
+    reply.send(err);
+  }
+});
+
+
+fastify.register(async function (fastify, opts) {
+  fastify.addHook('preHandler', async (request, reply) => {
+    try {
+      let model = request.body;
+      await request.jwtVerify(model.Token);
+    }
+    catch (err) {
       reply.send(err);
-  }
+    }
   });
 
-
-fastify.register(async function(fastify, opts){
-  fastify.addHook('preHandler', async(request, reply) => {
-      try{
-          let model=request.body;
-          await request.jwtVerify(model.Token);
-      }
-      catch(err){
-          reply.send(err);
-      }
-  });
-
-// Lettura dati dal simulatore e salvataggio su influx DB
-fastify.post('/api/busdati/', async (request, reply) => {
-  var dati=request.body;
-  //console.log("Dati: ",dati);
-  if(dati.Apertura==''){
-    influx.writePoints([
-      {
-        measurement: 'Posizione',
-        tags: { idBus: dati.IdVeicolo },
-        fields: { latitudine:dati.Latitudine, longitudine:dati.Longitudine },
-        timestamp:dati.DataOraBus+"00",
-      }
+  // Lettura dati dal simulatore e salvataggio su influx DB
+  fastify.post('/api/busdati/', async (request, reply) => {
+    var dati = request.body;
+    //console.log("Dati: ",dati);
+    if (dati.Apertura == '') {
+      influx.writePoints([
+        {
+          measurement: 'Posizione',
+          tags: { idBus: dati.IdVeicolo },
+          fields: { latitudine: dati.Latitudine, longitudine: dati.Longitudine },
+          timestamp: dati.DataOraBus + "00",
+        }
       ]);
-  }
-  else{
-    var porta;
-    if(dati.Apertura==1){
-       porta='Aperta';
     }
-    else{
-      porta='Chiusa';
-    }
-    influx.writePoints([
-      {
-        measurement: 'Apertura',
-        tags: { idBus: dati.IdVeicolo },
-        fields: { Apertura:porta, nPersone:dati.Conta_Persone, latitudine:dati.Latitudine, longitudine:dati.Longitudine },
-        timestamp:dati.DataOraPorte+"00",
+    else {
+      var porta;
+      if (dati.Apertura == 1) {
+        porta = 'Aperta';
       }
+      else {
+        porta = 'Chiusa';
+      }
+      influx.writePoints([
+        {
+          measurement: 'Apertura',
+          tags: { idBus: dati.IdVeicolo },
+          fields: { Apertura: porta, nPersone: dati.Conta_Persone, latitudine: dati.Latitudine, longitudine: dati.Longitudine },
+          timestamp: dati.DataOraPorte + "00",
+        }
       ]);
-  }
+    }
 
-  reply.status(201).send("201");
+    reply.status(201).send("201");
 
-  
+
   });
 });
 
 // visualizzazione dei dati 
-/*
-fastify.get('/api/visbus/:id', async (request, reply) => {
-  var id=request.params.id;
-  console.log(id);
-  var results;
-  influx.query('select * from Posizione where idBus='+id+' ').then(results => {
-    //return results;
-    console.log(results);
-  });
-  //reply.send(200).status("ok");
 
-  return results;
-  
-});
+/*
+1) visualizzare posizione di un determinato autobus:
+2) numero di persone di un determinato autobus
 */
 
+fastify.get('/api/visbus/:id', async (request, reply) => {
+  var id = request.params.id;
+  console.log(id);
+  var results;
+  var query = 'select * from Posizione where idBus=\'' + id + '\' ';
+
+  try{
+    influx.query(query).then(results => {
+      //console.log("sono qui");
+      if (results==''){
+       // console.log("sono qui?");
+        reply.status(404).send("Autobus not found");
+      }
+      else{
+      reply.send(results);
+      }
+    });
+  }
+  catch(error){
+    reply.status(404).send(error);
+  }
+  
+});
+/*
+3) numero di persone di un determinato autobus
+*/
+fastify.get('/api/numpers/:id', async (request, reply) => {
+  var id = request.params.id;
+  console.log(id);
+  var results;
+  var query = 'select nPersone from Apertura where idBus=\'' + id + '\' ';
+
+  
+    influx.query(query).then(results => {
+      //console.log("sono qui");
+      if (results==''){
+        // console.log("sono qui?");
+         reply.status(404).send("Autobus not found");
+       }
+       else{
+       reply.send(results);
+       }
+    });
+});
+/*
+2) visualizzare gli autobus online
+*/
 const start = async () => {
-  try {                                                       
-    await fastify.listen(obj.api.port,obj.api.ip)                                                    //Creo web server e sto in ascolto sulla porta 3000
+  try {
+    await fastify.listen(obj.api.port, obj.api.ip)                                                    //Creo web server e sto in ascolto sulla porta 3000
     fastify.log.info(`server listening on ${fastify.server.address().port}`)      // Ascolto tutte richiest http
   } catch (err) {
     fastify.log.error(err)
